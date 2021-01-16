96d23624a5eb780f4ef0acd9ad3ceeee3ee02503
dsong04 <davissong01@gmail.com>
2021-01-16 13:13:02 -0800

    Initial Navbar Push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// USING ES6 MODULES
+// <npm install react-router-dom> to get these
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// STYLE
+import "./App.css";
+
+// COMPONENTS
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>{/* <Route path="/" exact /> */}</Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+
+function Navbar() {
+  // menu button reversing functionality
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const [button, setButton] = useState(true);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  // ensures that Sign Up button is only rendered once
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  const closeMobileMenu = () => setClick(false);
+
+  //prettier-igmpre
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            {/* navbar logo, pulled from fontawesome.com */}
+            REFRY
+            <i className="fas fa-utensils" />
+          </Link>
+
+          {/* "hamburger" menu icon */}
+          <div className="menu-icon" onClick={handleClick}>
+            {/* adds menu icon, once clicked it will turn into an X */}
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+
+          {/* home page */}
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+
+            {/* about page */}
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+
+            {/* our mission */}
+            <li className="nav-item">
+              <Link
+                to="/our-mission"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Our Mission
+              </Link>
+            </li>
+
+            {/* search */}
+            <li className="nav-item">
+              <Link
+                to="/search"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Search
+              </Link>
+            </li>
+          </ul>
+
+          {button && <Button buttonStyle="btn--outline">SEARCH</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
