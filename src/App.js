d4121c3ce078db88a3189c6959f6fe9693e73788
dsong04 <davissong01@gmail.com>
2021-01-16 14:30:35 -0800

    Added pages (Home, About, Recipes), moved cards to Recipes page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 // <npm install react-router-dom> to get these
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Recipes from "./components/pages/Recipes.js";
 
 // STYLE
 import "./App.css";
 
 // COMPONENTS
 import Navbar from "./components/Navbar";
+import About from "./components/pages/About";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/about" component={About} />
+        <Route path="/recipes" component={Recipes} />
       </Switch>
     </Router>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../App.css";
 import HeroDynamic from "../HeroDynamic";
-import Cards from "../Cards";
+// import Cards from "../Cards";
 
 function Home() {
   return (
     <>
       <HeroDynamic />
-      <Cards />
+      {/* <Cards /> 
+      CARDS MOVED TO RECIPES PAGE!*/}
     </>
   );
 }
